64528b05973600fa4e6ee4cfd42a3206a15cb1df
Benni <miuta.beni@gmail.com>
2025-09-22 17:33:00 +0200

    api link
    api link

--- students-platform/frontend/vite.config.ts
+++ students-platform/frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://api:3000",
+        target: "http://localhost:3000",
         changeOrigin: true
       }
     }
